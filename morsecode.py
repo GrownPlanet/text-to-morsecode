dbf154c580a5b6603d2a487dd4e0aebecbc7f738
GrownPlanet <114773773+GrownPlanet@users.noreply.github.com>
2022-09-30 21:44:32 +0200

    Add files via upload

---
+++ morsecode.py
@@ -0,0 +1,13 @@
+text_to_code = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ' ': '    '}
+code = []
+
+print('special characters can not be in the text, they wil be automaticly removed')
+
+while True:
+    text = input('translate text: ')
+    for i in text:
+        if i in text_to_code:
+            code.append(text_to_code[i])
+    joinder_code = ''.join(code)
+    print(joinder_code)
+    code = []
